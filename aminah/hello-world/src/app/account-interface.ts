5d92ca986a5abb72c35374ea355f9fa303c932c0
amirahaminah <sitinuramirahaminah@gmail.com>
2020-06-19 11:55:03 +0800

    task1-aminah

---
+++ aminah/hello-world/src/app/account-interface.ts
@@ -0,0 +1,10 @@
+/* create interface with properties and data type to use in AccountComponent */
+export interface Account{
+    acctype: string,
+    accTypeDesc: null,
+	productType: string,
+	accNumber: string,
+	accStatus: string,
+	amount: number,
+	currencyCode: string 
+};
---
+++ aminah/hello-world/src/app/customer-interface.ts
@@ -0,0 +1,6 @@
+/* create interface with properties and data type to use in CustomerComponent */
+export interface Customer{
+    name: string,
+    nric: string,
+    branch: string;
+}
